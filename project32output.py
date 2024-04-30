75b9f20c188b63e73f7678e64a8ce3eddb5fc396
Jayloneal <121124684+Jayloneal@users.noreply.github.com>
2024-04-30 17:39:37 -0400

    Taking Screenshots Using Pyscreenshot

---
+++ project32output.py
@@ -0,0 +1,23 @@
+import pyscreenshot 
+  
+# To capture the screen 
+image = pyscreenshot.grab() 
+  
+# To display the captured screenshot 
+image.show() 
+
+# To save the screenshot 
+image.save("screenshot.png") 
+
+# Program for partial screenshot 
+  
+import pyscreenshot 
+  
+# im=pyscreenshot.grab(bbox=(x1,x2,y1,y2)) 
+image = pyscreenshot.grab(bbox=(10, 10, 500, 500)) 
+  
+# To view the screenshot 
+image.show() 
+  
+# To save the screenshot 
+image.save("screenshot.png") 
